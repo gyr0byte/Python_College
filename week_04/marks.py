f13adaf6e48b969e1c7102fecd9cefae67a5a035
Gauravlovescode <gqurav69@gmail.com>
2026-03-11 22:40:39 +0545

    Add script to calculate highest, lowest, total, and average marks

---
+++ week_04/marks.py
@@ -0,0 +1,18 @@
+name = input("What is your name? ")
+n = int(input("How many numbers will you like to input? "))
+marks = []
+for i in range(n):
+    m = int(input(f"Enter the {i+1} subject marks"))
+    marks.append(m)
+    
+highest = marks[0]
+lowest = marks[0]
+totalmarks = 0
+for mark in marks:
+    if highest < mark:
+        highest = mark
+    elif lowest > mark:
+        lowest = mark
+    totalmarks += mark                     
+
+average = totalmarks / n
