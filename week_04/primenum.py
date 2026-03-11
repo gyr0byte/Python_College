c1ee5a0db37f6916c33c4680f9f88e0c9d1b011a
Gauravlovescode <gqurav69@gmail.com>
2026-03-11 14:34:22 +0545

    Refactor prime number check logic and improve input validation

--- week_04/primenum.py
+++ week_04/primenum.py
@@ -1,9 +1,14 @@
-num = int(input("Enter you number: "))
-if num <= 0:
-    print(f"The Number cannot be 0 or Negative.")
+def pn(num):
+    if num < 1:
+        print(f"The Number cannot be Negative or Zero")
 
-elif num%2 == 0 or num%3 == 0:
-    print(f"The Number is not a Prime.")
-
-else:
-    print(f"The Number is a Prime")
+    for i in range(2,num):
+        if(num%i==0):
+            print("The Number is not a Prime.")
+            break
+    
+    else:
+        print(f"The Number is a Prime.")
+    
+num = int(input("Enter your Number: "))
+pn(num)
