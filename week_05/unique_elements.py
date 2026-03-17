4492bf32363fece34553ce9f9f89cf23b3496c5d
Gauravlovescode <gqurav69@gmail.com>
2026-03-17 11:15:47 +0545

    Refactor unique_elements.py to initialize revnums and adjust print statements for clarity

--- week_05/unique_elements.py
+++ week_05/unique_elements.py
@@ -1,10 +1,12 @@
 nums = [1,1,2,3,4,5,5,6,6,7,7]
-
+revnums = []
 newnums = []
 for i in range(0, len(nums)):
     if nums[i] in newnums:
         continue
     else:
         newnums.append(nums[i])
-
+for i in range(1, len(newnums)+1):
+    revnums.append(newnums[-i])
 print(newnums)
+print(revnums)
