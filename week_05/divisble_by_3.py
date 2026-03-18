9899166732ec4718a7bfc278c5eb81db4d3824a5
Gauravlovescode <gqurav69@gmail.com>
2026-03-18 14:44:07 +0545

    Update divisble_by_3.py to filter numbers divisible by both 3 and 2; add combined.py with sample lists

--- week_05/divisble_by_3.py
+++ week_05/divisble_by_3.py
@@ -1,7 +1,7 @@
 a = [2, 3, 6, 8, 9, 12, 15, 18, 24, 22, 33, 112]
 newlist = []
 for n in a:
-    if n % 3 == 0:
+    if n % 3 == 0 and n % 2 == 0:
         newlist.append(n)
     else:
         continue
