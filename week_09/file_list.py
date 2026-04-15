3f800606ed04089c288bca7184068a9dd4f9e73a
Gauravlovescode <gqurav69@gmail.com>
2026-04-15 14:51:25 +0545

    Remove unnecessary blank lines in file_list.py for cleaner code

--- week_09/file_list.py
+++ week_09/file_list.py
@@ -3,17 +3,13 @@ def write(path):
         data = file.write(
             "ID001, Harry, 85\nID002, Bob, 78\nID003, Shyam, 92")
 
-
 lst = []
-
-
 def read(path):
     with open(path, "r") as file:
         for line in file:
             lst.append(line.strip().split(", "))
     print(lst)
-
-
+    
 path = "./week_09/data.txt"
 write(path)
 read(path)
