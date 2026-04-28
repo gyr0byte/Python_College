bbbb4687aaf7d6f19fe174cab092460c0c413cd1
gyr0byte <gqurav69@gmail.com>
2026-04-28 10:53:14 +0545

    Refactor file_create function in datetime.py for improved readability

--- week_10/datetime.py
+++ week_10/datetime.py
@@ -1,16 +1,18 @@
-import datetime as dt 
+import datetime as dt
 
-# Get the current date and time
+
 def file_create():
     now = dt.datetime.now()
     print("Current date and time:", now)
 
-    # Create a filename with the current date and time
+
     filename = now.strftime("file_%Y-%m-%d_%H-%M-%S.txt")
     print("Generated filename:", filename)
 
-    # Create a file with the generated filename
     with open(filename, 'w') as file:
-        file.write("This file was created on " + now.strftime("%Y-%m-%d at %H:%M:%S"))
+        file.write("This file was created on " +
+                   now.strftime("%Y-%m-%d at %H:%M:%S"))
 
+
+file_name = "./week_10/"
 print(file_create())
